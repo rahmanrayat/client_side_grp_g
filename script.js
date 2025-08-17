d88770f37454f9fe30449ead1e26ae29bc7040db
Himal754 <Poudel-H@ulster.ac.uk>
2025-08-17 22:58:47 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 
+$(document).ready(function () {
+    $('#contactForm').on('submit', function (e) {
+        e.preventDefault();
+
+        var name = $('#name').val();
+        var email = $('#email').val();
+        var message = $('#message').val();
+
+        if (name == '') {
+            alert('Please enter your name');
+            return false;
+        }
+
+        if (email == '') {
+            alert('Please enter your email');
+            return false;
+        }
+
+        if (message == '') {
+            alert('Please enter a message');
+            return false;
+        }
+
+        let successMessage = '<div style="color: green; padding: 10px;">Form is Submitted Successfully we will contact you soon</div>';
+        $('#successMessage').html(successMessage);
+
+        $('#contactForm')[0].reset();
+    });
+});
